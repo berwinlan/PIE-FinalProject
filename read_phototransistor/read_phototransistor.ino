3beb0771aa41d5d3238cfe36dcf744fb3ffef597
Berwin Lan <blan@olin.edu>
2021-12-08 22:02:17 -0500

    Fix servo-phototransistor

--- read_phototransistor/read_phototransistor.ino
+++ read_phototransistor/read_phototransistor.ino
@@ -1,58 +1,59 @@
 /*
+ * Moves a servo based on phototransistor.
  * Code from Sweep sample code used
  */
 
 #include <Servo.h>
+#include <movingAvg.h>
 
 #define BAUD_RATE 115200
-#define LOOP_INTERVAL 20
-#define SERVO_PIN 1             // D1
+#define SERVO_PIN 9             // D9
 #define PHOTOTRANSISTOR_PIN A0
-#define THRESHOLD 5
-#define DELAY 5
+#define THRESHOLD 10            // light/dark threshold
+#define DELAY 900
+
+// Servo positions of head
+#define OUT_POS 0
+#define IN_POS 180
 
 Servo myservo;  // create servo object to control a servo
+movingAvg phototransistor(10);      // use 10 data points for moving avg
 
-int pos = 0;    // variable to store the servo position
-uint32_t loop_time;
 bool head_in;      // toggle based on head position
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(BAUD_RATE);
-  loop_time = millis();
   head_in = true;
   
   myservo.attach(SERVO_PIN);
-  myservo.write(0);         // initial position of head in
+  myservo.write(IN_POS);         // initial position of head in
+  
+  phototransistor.begin();
+  phototransistor.reset();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   uint32_t t;
-  uint16_t res;
+  uint16_t sensorData;
+  uint16_t sensorMovingAvg;
 
   t = millis();
-  res = analogRead(PHOTOTRANSISTOR_PIN);
-//  Serial.println(res);
+  sensorData = analogRead(PHOTOTRANSISTOR_PIN);
+  sensorMovingAvg = phototransistor.reading(sensorData);
+//  Serial.print("data reading: ");
+//  Serial.println(sensorData);
+//  Serial.print("moving avg: ");
+  Serial.println(sensorMovingAvg);
 
-  loop_time = t;
-
-  if ((res <= THRESHOLD) & head_in) {
-    for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees in steps of 1 degree
-      Serial.print(head_in);
-      Serial.println(pos);
-      myservo.write(pos);                 // tell servo to go to position in variable 'pos'
-      delay(DELAY);                       // waits for the servo to reach the position
-    }
-    head_in = false;
-  } else if ((res > THRESHOLD) & !head_in) {
-    for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-      Serial.print(head_in);
-      Serial.println(pos);
-      myservo.write(pos);                 // tell servo to go to position in variable 'pos'
-      delay(DELAY);                       // waits for the servo to reach the position
-    }
-    head_in = true;
+  if ((sensorMovingAvg <= THRESHOLD) && head_in) {
+    myservo.write(OUT_POS);
+    delay(DELAY);
+    head_in = !head_in;
+  } else if ((sensorMovingAvg > THRESHOLD) && !head_in) {
+    myservo.write(IN_POS);
+    delay(DELAY);
+    head_in = !head_in;
   }
 }
